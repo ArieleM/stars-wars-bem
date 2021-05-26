3582900c5e7bc42841f6ea51de42878716709fd6
Ariele Mendonça <ariele_apm@outlook.com>
2021-05-26 00:49:03 -0300

    Adiciona as sections

---
+++ src/views/pages/components/float/floatImage.js
@@ -0,0 +1,8 @@
+import yodaBaby from '../../../../assets/images/elements/baby-yoda.png'
+let floatImage = /*html */`
+  <div class="main-left">
+    <img src=${yodaBaby} alt="Yoda Baby" width="120px" heigth="auto" />
+  </div>
+`
+
+export default floatImage;
