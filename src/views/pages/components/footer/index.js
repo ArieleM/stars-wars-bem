bc76eb66f6eb47f9bb209a27486f380f03aee7fd
Ariele Mendonça <ariele_apm@outlook.com>
2021-05-26 00:49:59 -0300

    Inicia a estilização

---
+++ src/views/pages/components/footer/index.js
@@ -0,0 +1,13 @@
+import planet from '../../../../assets/images/elements/planet.png'
+
+let footer = /*html */`
+  <div>
+    <p>
+      Star wars  -  este é um exemplo criado para os Alunos Gama Academy
+      Módulo de boas práticas.
+    </p>
+    <img src=${planet} alt="Planet" heigth="180px" width="auto" />
+  </div>
+`
+
+export default footer;
